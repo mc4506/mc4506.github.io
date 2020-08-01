7ff51db0ed6102e3f062e5785b7e4a9d99927b9b
Mike S Chen <mike.sihong.chen@gmail.com>
2020-08-01 14:17:00 -0400

    revamped layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,138 +1,52 @@
-$(document).ready(function () {
-  let turnstileEl = $('.turnstile');
-  turnstileEl.on('click', animateClickTurnstile);
+$(document).ready(function() {
+    const turnstileEl = $('.turnstile');
+    turnstileEl.on('click', animateClickTurnstile);
 
-  let collapseBtn = $('.collapseBtn');
-  collapseBtn.on('click', animateClickCollapse);
+    const projectEl = $('.project');
+    projectEl.on('click', animateClickProject);
 
-  let headerSwitch = $('#header-switch');
-  let footerSwitch = $('#footer-switch');
-
-  headerSwitch.on('click', toggleAll);
-  footerSwitch.on('click', toggleAll);
-
-  let savedSticky = localStorage.getItem("sticky") || "";
-  $('#sticky').text(savedSticky);
-  getSavedSticky();
-
-  $(window).resize(()=>getSavedSticky());
-  
-  $('#sticky-note').resizable({
-    containment: "parent",
-    minWidth: 220,
-    minHeight: 160
-  });
-
-  $('#sticky-note').draggable({
-    containment: "parent",
-  });
-  
-  $('#sticky-note').on("drag", function(event,ui){
-    let position = $('#sticky-note').position();
-    localStorage.setItem("sticky-position", JSON.stringify(position));
-  });
-  $('#sticky-note').on("resize", function(event, ui){
-    let width = $('#sticky-note').width();
-    let height = $('#sticky-note').height();
-    let stickySize = [width, height];
-    localStorage.setItem("sticky-size", JSON.stringify(stickySize));
-  });
-
-  let saveStickyBtn = $('#save-sticky');
-  saveStickyBtn.on('click', saveSticky);
 });
 
-const getSavedSticky = function(){
+const animateClickProject = function(event) {
+    // console.log($(this));
+    const project = $(this).attr('data-product');
+    // console.log(project);
+    $(this).parents('.wrapper').css("animation", "collapse 0.35s ease-out both");
+    $(this).parents('.wrapper').width(32);
+    $(this).parents('.content').siblings('.turnstile').css("animation", "turn-down 0.3s ease-out both");
+    $(this).parents('.content').siblings('.turnstile').css("cursor", "pointer");
+    $(this).parents('.content').css("display", "none");
+    // $(this).parents('.wrapper').css("overflow", "hidden");
+    $(this).parents('.wrapper').css("border-left", "1px solid #b6b6b6");
+    $(this).parents('.wrapper').css("border-right", "1px solid #b6b6b6");
 
-  let savedStickySize = JSON.parse(localStorage.getItem("sticky-size")) || [220, 160];
-  if(savedStickySize[0]>$('.main-container').innerWidth()){
-    $('#sticky-note').width($('.main-container').innerWidth()-42);
-    $('#sticky-note').height(savedStickySize[1]);
-  } else{
-    $('#sticky-note').width(savedStickySize[0]);
-    $('#sticky-note').height(savedStickySize[1]);
-  };
-
-  let savedStickyPosition = JSON.parse(localStorage.getItem("sticky-position")) || [42, 42];
-  if(savedStickyPosition.left < 42 || savedStickyPosition.left > $('.main-container').innerWidth()){
-    $('#sticky-note').css("top", savedStickyPosition.top);
-    $('#sticky-note').css("left", 42); 
-  } else {
-    $('#sticky-note').css("top", savedStickyPosition.top);
-    $('#sticky-note').css("left", savedStickyPosition.left);
-  }
-  // console.log($('.main-container').innerWidth());
-  // console.log({"width": $('#sticky-note').width(), "height": $('#sticky-note').height(), "top": $('#sticky-note').css("top"), "left": $('#sticky-note').css("left")});
-};
-
-const saveSticky = () => {
-  let text = $('#sticky').val();
-  localStorage.setItem("sticky", text);
-  $('.sticky-label').text("Sticky - Saved to Local Storage");
-  setTimeout(() => {
-    $('.sticky-label').text("Sticky");
-  }, 1000);
-};
-
-
-let toggle = false;
-const toggleAll = function () {
-  if (!toggle) {
-    $('.turnstile').each(function () {
-          $(this).parent().css("animation", "collapse 0.35s ease-out both");
-          $(this).parent().width(32);
-          $(this).css("animation", "turn-down 0.3s ease-out both");
-          $(this).siblings('.collapseBtn').css("visibility", "hidden");
-          $(this).next().css("visibility", "hidden");
-          $(this).parent().css("overflow", "hidden");
-          $(this).parent().css("box-shadow", "2px 0px 5px 0px rgba(78, 78, 78, 0.75");
-    });
-    toggle = true;
-  } else {
-    $('.turnstile').each(function () {
-        $(this).parent().css("animation", "expand 0.35s ease-out both");
-        $(this).parent().width(300);
-        $(this).css("animation", "turn-up 0.3s ease-out both");
-        $(this).next().css("visibility", "visible");
-        $(this).parent().css("overflow", "auto");
-        $(this).siblings('.collapseBtn').css("visibility", "visible");
-        $(this).parent().css("box-shadow", "none")
-   });
-   toggle = false;
-  }
-};
-
-function animateClickCollapse() {
-  let parentWidth = $(this).parent().width();
-  if (parentWidth > 100) {
-    $(this).parent().css("animation", "collapse 0.35s ease-out both");
-    $(this).parent().width(32);
-    $(this).siblings('.turnstile').css("animation", "turn-down 0.3s ease-out both");
-    $(this).css("visibility", "hidden");
-    $(this).prev().css("visibility", "hidden");
-    $(this).parent().css("overflow", "hidden");
-    $(this).parent().css("box-shadow", "2px 0px 5px 0px rgba(78, 78, 78, 0.75");
-  }
+    displayProject(project);
 }
 
-function animateClickTurnstile() {
-  let parentWidth = $(this).parent().width();
-  if (parentWidth > 100) {
-    $(this).parent().css("animation", "collapse 0.35s ease-out both");
-    $(this).parent().width(32);
-    $(this).css("animation", "turn-down 0.3s ease-out both");
-    $(this).siblings('.collapseBtn').css("visibility", "hidden");
-    $(this).next().css("visibility", "hidden");
-    $(this).parent().css("overflow", "hidden");
-    $(this).parent().css("box-shadow", "2px 0px 5px 0px rgba(78, 78, 78, 0.75");
-  } else
-  if (parentWidth <= 100) {
+const clearProjects = function() {
+    $('.product-card').css("display", "none");
+}
+
+const displayProject = function(id) {
+    let project = $(`#${id}`);
+    // console.log(project);
+    setTimeout(function(){
+        project.css("display", "flex");
+    }, 300);
+}
+
+
+const animateClickTurnstile = function() {
+    const $this = $(this);
     $(this).parent().css("animation", "expand 0.35s ease-out both");
     $(this).parent().width(300);
     $(this).css("animation", "turn-up 0.3s ease-out both");
-    $(this).next().css("visibility", "visible");
-    $(this).parent().css("overflow", "auto");
-    $(this).siblings('.collapseBtn').css("visibility", "visible");
-    $(this).parent().css("box-shadow", "none")
-  }
+    $(this).css("cursor", "default");
+    // $(this).parent().css("overflow", "auto");
+    $(this).parent().css("border", "none");
+    setTimeout(function() {
+        $this.next().css("display", "block");
+    }, 200);
+    clearProjects();
+
 }
